f9cf3abee8531dbc426ea43782dbc95f8001d905
Marco Truffat <marcotruffat@paucasesnovescifp.cat>
2021-02-05 17:20:05 +0100

    Created class Product to represent objects from Product table with and id and size.

---
+++ DI03ControlLibrary/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI03ControlLibrary
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string Size { get; set; }
+    }
+}
